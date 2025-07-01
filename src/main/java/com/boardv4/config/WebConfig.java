ac8bfa90eda22d712322adc354f56904a3a688b5
kdh940 <kdh940@gmail.com>
2025-07-01 18:36:57 +0900

    CORS 설정 추가 및 MYSQL 인코딩 수정

--- src/main/java/com/boardv4/config/WebConfig.java
+++ src/main/java/com/boardv4/config/WebConfig.java
@@ -1,7 +1,7 @@
 package com.boardv4.config;
 
 import com.boardv4.security.LoginUserArgumentResolver;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,10 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-@AllArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOriginsProperty;
     private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    public WebConfig(LoginUserArgumentResolver loginUserArgumentResolver) {
+        this.loginUserArgumentResolver = loginUserArgumentResolver;
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -23,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOriginsProperty.split(","))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Content-Disposition"); //파일명 접근을 위해서
